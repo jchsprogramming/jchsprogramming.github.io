a958c53285b5108574a0cc040143b3490e37932e
JCHS Programming Club <106827010+jchsprogramming@users.noreply.github.com>
2022-06-14 17:20:26 -0400

    Create blank React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles/reset.css';
+import './styles/display.css';
+import './styles/theme.css';
+import './styles/style.css';
+import './styles/media.css';
+
+function App() {
+    return (
+        <div className="App">
+            
+        </div>
+    );
+}
+
+export default App;
