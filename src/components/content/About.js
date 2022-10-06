951357025e089515bbf521c981de9022ee1b2cd7
JCHS Programming Club <106827010+jchsprogramming@users.noreply.github.com>
2022-10-06 19:47:55 -0400

    Edit meeting times

--- src/components/content/About.js
+++ src/components/content/About.js
@@ -12,8 +12,11 @@ export default class About extends React.Component {
                 <h2>About</h2>
                 <div className="br"></div>
                 <p className="mobile-br">Welcome to James Caldwell High School's Programming Club!</p>
+                <div className="br"></div>
                 <p className="mobile-br">Students who have worked on the monthly project will display their unique take on a concept and students attending will have an opportunity to view other club members' projects and vote on our favorite project of the month.</p>
-                <textarea className="format full-width" type="text" value="Meetings are held every 1st Tuesday of the month after school in Room 206 (subject to change)." readOnly unselectable="on"></textarea>
+                <textarea className="format full-width" type="text" value="Meetings are usually held twice a month after school on Wednesdays in Room 206 (subject to change)." readOnly unselectable="on"></textarea>
+                <div className="mobile-br"></div>
+                <p className="mobile-br">View our full meeting schedule <a href="http://docs.google.com/spreadsheets/d/1oYR1C-ZYklAf3cjh7A9elWXlH31H47H8hB_EMMGPXZk/" target="_blank" rel="noopener noreferrer" className="underline clickable">here</a>!</p>
                 <div className="br"></div>
                 <p className="heading">Club Board</p>
                 <p>Jordan Sang — President</p>
